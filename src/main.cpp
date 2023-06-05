c99a9fbb6879768ac6e99b3a105c9f96cc0f07df
Mr. Rc <60568652+HACKE-RC@users.noreply.github.com>
2023-06-05 14:45:30 +0530

    added premature error checks for command line program arguments.

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,9 @@
 #include "JackTokenizer/JackTokenizer.hpp"
 
 int main(int argc, char* argv[]){
+    if (argc < 2){
+        return -1;
+    }
     JackTokenizer tokenizer(argv[1], "test.tst");
     tokenizer.cleanCode();
     tokenizer.tokenizeCode();
