ceb867ed669b7c9fc167436cd4a38d7cd297fc7d
Mr. Rc <60568652+HACKE-RC@users.noreply.github.com>
2023-09-27 14:16:37 +0530

    Input check added to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,8 @@
 
 int main(int argc, char* argv[]){
     if (argc < 2){
-        std::cerr << "Usage: jcomp.exe <file>" << std::endl;
+        std::cerr << "Usage: " << argv[0] << " <file or folder path>" << std::endl;
+        std::exit(-1);
     }
 
     CompilationEngine compiler(argv[1]);
